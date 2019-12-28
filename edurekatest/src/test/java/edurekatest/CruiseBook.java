73e3dcb9e3a30251811f62fe5f96856495d1e898
rober <rober@192.168.1.231>
2019-12-28 13:51:51 -0600

    DevOps Project Final

---
+++ edurekatest/src/test/java/edurekatest/CruiseBook.java
@@ -0,0 +1,15 @@
+package edurekatest;
+
+import org.testng.annotations.Test;
+
+@Test
+public class CruiseBook {
+	String url="http://www.newtours.demoaut.com/";
+	//System.setProperty("webdriver.chrome.driver", "C:\\Users\\rober\\eclipse-workspace\\edurekatest\\Drivers\\chromedriver.exe"); 
+	//WebDriver driver = new ChromeDriver();
+	//driver.manage().timeouts().pageLoadTimeout(30,TimeUnit.SECONDS);
+	//driver.navigate().to(url);
+	//System.out.println("Lauched Chrome browser with link "+url);
+	//driver.manage().window().maximize();
+
+}
